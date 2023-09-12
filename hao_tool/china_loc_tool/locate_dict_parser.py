6daa3c65d1d9fa6111293f9e7e41038b6a0991d0
zhifenghao123 <zhifenghao123@outlook.com>
2023-09-12 16:41:02 +0800

    修改包名

--- hao_tool/china_loc_tool/locate_dict_parser.py
+++ hao_tool/china_loc_tool/locate_dict_parser.py
@@ -2,7 +2,7 @@ import jionlp as jio
 
 
 def getLoc(request):
-    loc_list = jio.read_file_by_line("../../HaoTool/dataset/loc_list.txt")
+    loc_list = jio.read_file_by_line("../dataset/loc_list.txt")
     print(loc_list)
     return loc_list
 
