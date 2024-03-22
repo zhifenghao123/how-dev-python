550f11ccf431a349ccf5a21985e2891dc31be083
zhifenghao123 <zhifenghao123@outlook.com>
2024-03-22 19:29:23 +0800

    动态构造 DataFrame2

--- data_analysis/h_pandas/pandas_analysis1.py
+++ data_analysis/h_pandas/pandas_analysis1.py
@@ -63,9 +63,22 @@ def dynamic_construct_dataframe2():
         ]
     ]
 
+    indicators = [
+        [
+            {'indicator_name': "indicator1", 'indicator_val': 20},
+            {'indicator_name': "indicator2", 'indicator_val': 0.9}
+        ],[
+            {'indicator_name': "indicator1", 'indicator_val': 80},
+            {'indicator_name': "indicator2", 'indicator_val': 0.6}
+        ],[
+            {'indicator_name': "indicator1", 'indicator_val': 68},
+            {'indicator_name': "indicator2", 'indicator_val': 0.5}
+        ]
+    ]
+
     # 初始化一个空的DataFrame，列数取决于最大的内部列表长度
     max_length = max(len(binning) for binning in feature_binnings)
-    df = pd.DataFrame(index=range(len(feature_binnings)), columns=range(max_length))
+    df = pd.DataFrame(index=range(len(feature_binnings)), columns=range(max_length + 2))
 
     # 遍历每个内部列表，填充DataFrame
     for i, binning in enumerate(feature_binnings):
@@ -73,12 +86,16 @@ def dynamic_construct_dataframe2():
             # 填充非空值，忽略超出当前行长度的索引
             if j < max_length:
                 df.iat[i, j] = f"{item['name']}={item['value']}"
+        indicator = indicators[i]
+        for k, item in enumerate(indicator):
+            df.iat[i, max_length + k] = item['indicator_val']
 
-                # 删除包含NaN的列，只保留有数据的列
+
+    # 删除包含NaN的列，只保留有数据的列
     df = df.dropna(axis=1, how='all')
 
     # 重新命名列以符合您的要求（如果需要）
-    df.columns = [f"feature_{col + 1}" for col in df.columns]
+    # df.columns = [f"feature_{col + 1}" for col in df.columns]
 
     # 显示最终的DataFrame
     print(df)
@@ -88,8 +105,6 @@ def dynamic_filter_dataframe():
     动态筛选 DataFrame
     :return:
     """
-    import pandas as pd
-
     # 假设这是你的DataFrame
     df = pd.DataFrame({
         'cmid': [1, 2, 3, 4, 5],
