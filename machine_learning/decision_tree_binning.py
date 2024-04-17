cfa1c1df69942af21ef033c26c8b50eaf442b3cb
zhifenghao123 <zhifenghao123@outlook.com>
2024-04-17 20:51:03 +0800

    读取数据集

--- machine_learning/decision_tree_binning.py
+++ machine_learning/decision_tree_binning.py
@@ -19,6 +19,25 @@ def read_data_set():
     # data_x, data_y = make_classification(n_samples=10000, n_classes=4, n_features=10, n_informative=8, random_state=0)
     return data_X, data_Y
 
+def read_data_set2():
+    """
+    读取数据集
+    :return:
+    """
+    data = {
+        'id': np.arange(1, 101),
+        'gender': np.random.choice(['Male', 'Female'], size=100),
+        'age': np.random.randint(0, 101, size=100),
+        'career': np.random.choice(['教师', '医生', '警察', '软件开发工程师', '会计', '公务员', '工人'], size=100),
+        'incomeAmt': np.random.uniform(100, 100000, size=100),
+        'label': np.random.randint(0, 2, size=100)
+    }
+    df_data = pd.DataFrame(data)
+    data_X = df_data.loc[:, 'career':'career']
+    data_Y = df_data['label']
+    # data_x, data_y = make_classification(n_samples=10000, n_classes=4, n_features=10, n_informative=8, random_state=0)
+    return data_X, data_Y
+
 
 def decision_tree_tranning(data_X, data_Y, max_leaf_nodes_val):
     """
@@ -95,6 +114,6 @@ if __name__ == '__main__':
     max_bin = 4
     data_X, data_Y = read_data_set()
     decision_tree = decision_tree_tranning(data_X, data_Y, max_bin)
-    # show_decision_tree(decision_tree)
+    show_decision_tree(decision_tree)
     bin_result = decision_tree_binning(decision_tree, max_bin, data_X, data_Y)
     binning_result_view(data_X, bin_result)
