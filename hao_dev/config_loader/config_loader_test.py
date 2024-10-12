0faa6417d30bf4ad5f114ae17dbb329f75edf90d
zhifenghao123 <zhifenghao123@outlook.com>
2024-10-12 11:34:05 +0800

    config_loader

--- hao_dev/config_loader/config_loader_test.py
+++ hao_dev/config_loader/config_loader_test.py
@@ -4,3 +4,13 @@ if __name__ == '__main__':
     print(MYSQL_CFG)
     print(INFLUX_DB_CFG)
     print(NOTIFY_CFG)
+
+    extra_info2 = NOTIFY_CFG['EXTRA_INFO2']
+    print(extra_info2)
+    print("------------------")
+    print("extra_info2[\"1\"]:" + extra_info2["1"])
+    temp = 1
+    temp = str(temp)
+    print("extra_info2[1]:" + extra_info2[temp])
+
+
